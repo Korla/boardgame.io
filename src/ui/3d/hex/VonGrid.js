9c7e54371ee10c44c2fe0a91720f753d52d3ab7d
korla <bengtsson.johannes@gmail.com>
2018-10-15 10:03:22 +0200

    Fix warning that property has been renamed

--- src/ui/3d/hex/VonGrid.js
+++ src/ui/3d/hex/VonGrid.js
@@ -199,7 +199,7 @@ export class VonGrid {
 
     let geo = this._geoCache[height];
     if (!geo) {
-      this.extrudeSettings.amount = height;
+      this.extrudeSettings.depth = height;
       geo = new THREE.ExtrudeGeometry(this.cellShape, this.extrudeSettings);
       this._geoCache[height] = geo;
     }
@@ -233,7 +233,7 @@ export class VonGrid {
       cellSize: this.cellSize,
       material: null,
       extrudeSettings: {
-        amount: 1,
+        depth: 1,
         bevelEnabled: true,
         bevelSegments: 1,
         steps: 1,
